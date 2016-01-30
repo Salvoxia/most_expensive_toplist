c1be24aec30f25d608d6c02b369881b9a283b271
Salvoxia <Salvoxia@blindfish.info>
2016-01-30 16:38:24 +0100

    Initial commit of version 1.0

---
+++ most_expensive_toplist/init.php
@@ -0,0 +1,4 @@
+<?php
+$modInfo['most_expensive_toplist']['name'] = "Most Expensive Toplist";
+$modInfo['most_expensive_toplist']['abstract'] = "Adds alltime/monthly toplists for most expensive kills/losses to corp/alliance detail view";
+$modInfo['most_expensive_toplist']['about'] = "by <a href=\"http://gate.eveonline.com/Profile/Salvoxia\">Salvoxia</a>";
